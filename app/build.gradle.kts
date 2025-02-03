4a7cfd492d499c89b42dc235fd62c64c605e2b6f
lexynotfound <raihanardila@gmail.com>
2025-02-03 09:42:30 +0700

    update version 1.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.9"
+        versionName = "1.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
